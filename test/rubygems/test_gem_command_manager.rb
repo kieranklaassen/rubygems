99497038fd900c4ce7632cde06606e9ec14fc20e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-08-26 21:00:24 +0900

    Change the condition for did_you_mean with Ruby 2.3

--- test/rubygems/test_gem_command_manager.rb
+++ test/rubygems/test_gem_command_manager.rb
@@ -64,7 +64,7 @@ class TestGemCommandManager < Gem::TestCase
 
     message = 'Unknown command pish'.dup
 
-    if defined?(DidYouMean::SPELL_CHECKERS) && defined?(DidYouMean::Correctable)
+    if RUBY_VERSION > "2.3"
       message << "\nDid you mean?  \"push\""
     end
 
